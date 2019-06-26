9647ac8fd6c2807e865bedfe0ee9cdb2b7f98000
Daniel Haehre <dghaehre@gmail.com>
2019-06-26 02:23:46 +0200

    Add Today and Week. Still along way to go before working mvp

--- app/Display.hs
+++ app/Display.hs
@@ -17,3 +17,48 @@ listProjects d = do
   putStrLn "Your current projects:"
   _ <- mapM_ (putStrLn . File.name) $ projects d
   return Finish
+
+-- TODO
+-- Get total amount of jobs and total time spent
+totalJobs :: [Job] -> (Int, Int)
+totalJobs j = (length j, 0)
+
+concatJobs :: [Project] -> [Job]
+concatJobs = foldl (\b a -> jobs a ++ b) [] 
+
+displayToday :: Maybe [Project] -> IO Result'
+displayToday Nothing = return $ Err "No such project."
+displayToday (Just p) = do
+  putStrLn $ "Today (" ++ show jobs ++ ") - " ++ show time ++ "\n"
+  displayJobs p
+    where
+      (jobs, time) = totalJobs $ concatJobs p
+
+displayWeek :: Maybe [Project] -> IO Result'
+displayWeek Nothing = return $ Err "No such project."
+displayWeek (Just p) = do
+  putStrLn $ "This week: " ++ (show jobs) ++ "\n" 
+  displayJobs p
+    where
+      (jobs, time) = totalJobs $ concatJobs p
+
+displayJobs :: [Project] -> IO Result'
+displayJobs p = do
+  _ <- mapM_ d p
+  return Finish
+    where
+      d :: Project -> IO ()
+      d p = do
+        let l = show $ length $ jobs p
+        putStrLn $ File.name p ++ " (" ++ l ++ ") - " ++ "0"
+        _ <- mapM_ displayJob $ jobs p
+        return ()
+
+displayJob :: Job -> IO ()
+displayJob j = do
+  putStrLn $ File.description j
+
+displayProject :: [Project] -> IO Result'
+displayProject p = do
+  putStrLn "Display project"
+  return Finish
--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,7 @@ module Main where
 import System.IO
 import System.Console.CmdArgs
 import System.Console.CmdArgs.Implicit
+import Data.Maybe
 
 import File
 import Display
@@ -12,6 +13,8 @@ import Display
 data Argument = 
     Display { project :: String }
   | New     { project :: String }
+  | Today   { project :: String }
+  | Week    { project :: String }
   | List
   | Delete  { project :: String }
   | Start   { project :: String
@@ -22,6 +25,8 @@ data Argument =
 data Operation =
     Display' StoredData
   | New' StoredData String
+  | Today' StoredData String --
+  | Week' StoredData String --
   | Error' String
   | Delete' StoredData String
   | List' StoredData
@@ -29,6 +34,12 @@ data Operation =
 
 display :: Argument
 display = Display { project = def &= help "Project name" &= typ "name" }
+
+today :: Argument
+today = Today { project = def &= help "Project name" &= typ "name" }
+
+week :: Argument
+week = Week { project = def &= help "Project name" &= typ "name" }
 
 new :: Argument
 new = New 
@@ -42,19 +53,20 @@ start = Start
   }
 
 list :: Argument
-list = List  &= help "List out stored projects."
+list = List &= help "List out stored projects."
 
 delete :: Argument
 delete = Delete
   { project = def &= typ "<Project name>" &= argPos 0
   } &= help "Delete project"
 
-
 operation :: Argument -> Either String StoredData -> Operation
 operation _ (Left s)            = Error' s
 operation (New s) (Right p)     = New' p s
 operation List (Right p)        = List' p
 operation (Delete s) (Right p)  = Delete' p s
+operation (Today s) (Right p)   = Today' p s
+operation (Week s) (Right p)    = Week' p s
 operation _ (Right p)           = Display' p
 
 elemName :: String -> [Project] -> Bool
@@ -62,6 +74,40 @@ elemName s [] = False
 elemName s (x:xs) 
   | File.name x == s = True
   | otherwise = elemName s xs
+
+getProjects :: String -> StoredData -> Maybe [Project]
+getProjects p d
+  | length byName > 0 = Just byName
+  | p /= ""           = Nothing
+  | otherwise         = Just $ projects d
+  where
+    byName = filter (\x -> File.name x == p) $ projects d
+
+-- TODO
+filterToday :: String -> String -> Bool
+filterToday start end = True
+
+-- TODO
+filterWeek :: String -> String -> Bool
+filterWeek start end = True
+
+maybe' :: (a -> b) -> Maybe a -> Maybe b
+maybe' f a
+  | isJust a  = Just $ f $ fromJust a
+  | otherwise = Nothing
+
+getByTime :: (String -> String -> Bool) -> String -> StoredData -> Maybe [Project]
+getByTime f n = maybe' (filter noJobs . map (filterJobs f)) . getProjects n
+  where
+    noJobs x = 0 < length (File.jobs x)
+
+-- Remove jobs that is not relevant
+filterJobs :: (String -> String -> Bool) -> Project -> Project
+filterJobs f p = File.Project n filteredJobs
+  where
+    n             = File.name p
+    filteredJobs  = filter isRelevant $ jobs p
+    isRelevant j  = f (File.startTime j) (File.endTime j)
 
 createNew :: StoredData -> String -> IO Result'
 createNew d p 
@@ -80,22 +126,23 @@ deleteProject d p
         exist = elemName p (projects d)
         remove x = File.name x /= p
 
-
 runOperation :: Operation -> IO Result'
 runOperation (New' d s)     = createNew d s
 runOperation (List' d)      = listProjects d
 runOperation (Delete' d s)  = deleteProject d s
+runOperation (Today' d s)   = displayToday $ getByTime filterToday s d
+runOperation (Week' d s)    = displayWeek $ getByTime filterWeek s d
 runOperation _              = return $ Err "Operation not ready"
 
 record :: StoredData -> String -> String -> IO Result'
 record d project job = do
-  return Ok
+  return $ Err "Operation not ready"
    --where
     --go
 
 main :: IO ()
 main = do
-  input <- cmdArgs $ modes [new, start, display, list, delete]
+  input <- cmdArgs $ modes [new, start, display, list, delete, today, week]
     &= help "Track time spent on projects" 
     &= program "tt" 
     &= summary "Time tracker v0.1"
